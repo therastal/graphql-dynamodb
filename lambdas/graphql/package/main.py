89bae949302b9d543fa6b7e311893d32404ef661
Rastal <rastal@protonmail.com>
2023-03-07 17:59:10 -0600

    Remove extra FastAPI pathing and CORSMiddleware

--- lambdas/graphql/package/main.py
+++ lambdas/graphql/package/main.py
@@ -1,22 +1,15 @@
-import os
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from strawberry import Schema
-from strawberry.fastapi import GraphQLRouter
 from strawberry.extensions import ParserCache, ValidationCache
+from strawberry.fastapi import GraphQLRouter
 
 from . import schema
-
-DEPLOYMENT = os.getenv("DEPLOYMENT_KEY", "local")
 
 graphql_schema = Schema(schema.Query, extensions=[ParserCache(), ValidationCache()])
 graphql_app = GraphQLRouter(graphql_schema)
 
-app = FastAPI(root_path=f"/{DEPLOYMENT}")
+app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
-app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
-)
 
 handler = Mangum(app)
